05974376dc4f9ff5aa4ca3513410681cfab6fb4d
Jayashree <jayashree.a@cgi.com>
2019-07-26 17:55:56 +0530

    xmltask code updated

---
+++ src/main/java/com/stackroute/domain/App.java
@@ -0,0 +1,25 @@
+package com.stackroute.domain;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.rowset.spi.XmlReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie = context.getBean("movie",Movie.class);
+        System.out.println(movie.getActor());
+        XmlBeanFactory xmlBeanFactory= new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        Movie movie1 = context.getBean("movie",Movie.class);
+        System.out.println(movie1.getActor());
+    }
+}
